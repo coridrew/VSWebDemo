4527de60412044454ea704410f6ae9de49b9c309
Cori Drew <cori.drew@improvingenterprises.com>
2014-10-23 10:04:29 -0500

    created mvc project, added bootstrap less nuget package & enabled nuget package restore

---
+++ VisualStudioDemo.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VisualStudioDemo.Web.MvcApplication" Language="C#" %>
---
+++ VisualStudioDemo.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VisualStudioDemo.Web.Startup))]
+namespace VisualStudioDemo.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
